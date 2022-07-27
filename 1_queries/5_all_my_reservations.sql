b8e034f5e2f83425ec540ee235a123a39ffb2510
Leeyanhawrt <leeyan1108@gmail.com>
2022-07-27 13:31:59 +0000

    Add web app

--- 1_queries/5_all_my_reservations.sql
+++ 1_queries/5_all_my_reservations.sql
@@ -1,9 +1,8 @@
 SELECT reservations.id, properties.title, reservations.start_date, properties.cost_per_night, AVG(property_reviews.rating) AS average_rating
 FROM reservations 
 JOIN properties ON (reservations.property_id = properties.id)
-JOIN users ON (users.id = reservations.guest_id)
 JOIN property_reviews ON (property_reviews.property_id = properties.id)
-WHERE users.id = 1
+WHERE reservations.guest_id = 1
 GROUP BY reservations.id, properties.title, reservations.start_date, properties.cost_per_night
 ORDER BY reservations.start_date ASC
 LIMIT 10;
